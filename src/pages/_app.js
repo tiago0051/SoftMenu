87e61a1f7726c1da8f305083add14cdb93316474
Tiago Salgado <tiagosalgado2002@hotmail.com>
2021-08-12 18:23:48 -0300

    UPDATE PWA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import {light} from '../styles/theme';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={light}>
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
       <GlobalStyle/>
       <Component {...pageProps} />
     </ThemeProvider>
