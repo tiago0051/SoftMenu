8f9124da0c7fb021884e10ddae088d880ff5d847
Tiago Salgado <tiagosalgado2002@hotmail.com>
2021-08-12 21:58:00 -0300

    Mongoose

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mongoose from 'mongoose';
 
-export default function handler(req, res) {
+const uri = "mongodb+srv://app:YDmE4zf6RGHSiPEC@cluster0.kohrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+export default async function handler(req, res) {
+  if(mongoose.connection.readyState < 1)
+    await mongoose.connect(uri)
+
   res.status(200).json({ name: 'John Doe' })
 }
